6e334b47866f51bac3da627df5a10b095d1fb5ae
Bruno-Fernando <bruno.silva@ccc.ufcg.edu.br>
2019-06-06 22:00:37 -0300

    Drop script

---
+++ drop_ddl_turma03_grupo04.sql
@@ -0,0 +1,14 @@
+DROP TABLE Cliente cascade constraints;
+DROP TABLE Telefone;
+DROP TABLE Avaliacao cascade constraints;
+DROP TABLE Funcionario cascade constraints;
+DROP TABLE Quarto cascade constraints;
+DROP TABLE Equipamento;
+DROP TABLE Produto cascade constraints;
+DROP TABLE Dependente;
+DROP TABLE Quarto_Vende_Produto;
+DROP TABLE Funcionario_Mantem_Quarto;
+DROP TABLE Cliente_Reserva_Quarto;
+DROP TABLE Quarto_Hospeda_Cliente;
+
+
