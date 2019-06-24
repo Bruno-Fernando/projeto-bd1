7084240e77c2471a14587193f2a55f5a51bed925
Leonardomotta <Leonardo.mota@ccc.ufcg.edu.br>
2019-06-24 10:06:33 -0300

    2,7,12,16 atualizadas

--- respostas.sql
+++ respostas.sql
@@ -1,5 +1,4 @@
 -- Questão 2
-
 create view lista_cliente_qtdProd as
 select c.cpf from venda v inner join produto p on p.id = v.id_produto
 inner join quarto q on q.numero = v.numero_quarto
@@ -9,7 +8,56 @@ where v.data <= h.DIA_CHECK_OUT and v.data >= h.DIA_CHECK_IN
 group by c.cpf
 order by sum(v.QUANTIDADE)
 
-
-
-
 -- Questão 7
+select f.cpf from FUNCIONARIO f inner join
+MANUTENCAO m on m.CPF_FUNCIONARIO = f.cpf
+group by cpf
+having count(*)>2
+
+--Questão 12
+select q.numero , e.equipamento from quarto q left join
+equipamento e on e.numero_quarto = q.numero
+
+
+--Questão 16
+create or replace procedure atualizaPrecosProdutosByTipo(
+            prod_tipo in produto.tipo%TYPE,
+            desc_valor in produto.valor%TYPE
+   
+    ) as
+    
+    
+    
+   
+   BEGIN 
+     
+     declare 
+    
+    cursor att_preco is select * from produto where produto.tipo = prod_tipo;
+    res att_preco%rowtype;
+    desconto produto.valor%TYPE;
+    
+    begin
+       open att_preco;
+       
+       loop
+       
+       fetch att_preco into res;
+       exit when att_preco%NOTFOUND;
+       desconto := res.valor + (desc_valor*res.valor);
+   
+       update produto set valor = desconto
+        where id = res.id;
+        
+       END LOOP;
+       close att_preco;
+       commit;
+       
+     end;
+   
+   END;
+
+begin 
+ ATUALIZAPRECOSPRODUTOSBYTIPO('restaurante', -0.1);
+end;
+  
